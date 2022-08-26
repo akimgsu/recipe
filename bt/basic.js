54a483ab74404cba01a680a2ebf251ff600e4298
Abraham Kim <akim92@gsu.edu>
2022-08-25 21:10:55 -0400

    added tree

--- bt/basic.js
+++ bt/basic.js
@@ -5,40 +5,7 @@ Back Tracking (Decision Space)
 - Combinations
 - Sudoku...
 */
-/*
-https://leetcode.com/problems/subsets-ii/
-90. Subsets II
-*/
 
-const subsetsWithDup = function (nums) {
-    const result = [];
-    const subset = (path, idx) => {
-        result.push(path);
-        for (let i = idx; i < nums.length; i++) {
-            if (i !== idx && nums[i] === nums[i - 1]) continue;
-            subset([...path, nums[i]], i + 1);
-        }
-    }
-    subset([], 0);
-    return result;
-};
-console.log(subsetsWithDup([1, 2, 2]));
-/*
-https://leetcode.com/problems/subsets/
-78. Subsets
-*/
-const subsets = (nums) => {
-    const result = [];
-    const proc = (path, idx) => {
-        result.push(path)
-        for (let i = idx; i < nums.length; i++) {
-            proc([...path, nums[i]], i + 1);
-        }
-    }
-    proc([], 0);
-    return result;
-};
-// console.log(subsets([1, 2, 3]));
 
 const subsetsReduce = (nums) => {
     return nums.reduce((sets, num) => sets.forEach(set => sets.push([...set, num])) || sets, [[]]);
